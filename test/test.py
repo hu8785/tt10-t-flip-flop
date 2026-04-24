d074b846660c26e64e39777406c037e49f1c2bf6
hu8785 <patilaku96@gmail.com>
2026-04-24 16:45:23 -0400

    Update test.py
    done and push

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -8,33 +5,45 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
+    dut._log.info("Start T Flip Flop Test")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+
+    # Reset
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 5)
+    assert dut.uo_out[0].value == 0
+
+    # Release reset
     dut.rst_n.value = 1
+    dut.ui_in[0].value = 0
+    await ClockCycles(dut.clk, 2)
 
-    dut._log.info("Test project behavior")
+    # tin = 0, output should hold
+    q_before = dut.uo_out[0].value
+    await ClockCycles(dut.clk, 3)
+    assert dut.uo_out[0].value == q_before
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    # tin = 1, output should toggle
+    dut.ui_in[0].value = 1
     await ClockCycles(dut.clk, 1)
+    q1 = dut.uo_out[0].value
+    await ClockCycles(dut.clk, 1)
+    q2 = dut.uo_out[0].value
+    assert q1 != q2
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # qbar should be complement of q
+    assert dut.uo_out[1].value != dut.uo_out[0].value
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # tin = 0 again, output should hold
+    dut.ui_in[0].value = 0
+    q_hold = dut.uo_out[0].value
+    await ClockCycles(dut.clk, 3)
+    assert dut.uo_out[0].value == q_hold
+
+    dut._log.info("T Flip Flop Test Completed")
